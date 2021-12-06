5e0058ce9dfea0776f3b9c81b1d7753be792499a
Knight <82474150+RealKnight14@users.noreply.github.com>
2021-12-06 11:57:03 +0000

    V1
    Start Of Program Creation

---
+++ Gaming Interface/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProfile
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmProfile))
+        Me.GrpProfile = New System.Windows.Forms.GroupBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.txtPassword = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.lblUsername = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GrpProfile.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GrpProfile
+        '
+        Me.GrpProfile.BackColor = System.Drawing.Color.Transparent
+        Me.GrpProfile.Controls.Add(Me.btnExit)
+        Me.GrpProfile.Controls.Add(Me.btnLogin)
+        Me.GrpProfile.Controls.Add(Me.txtPassword)
+        Me.GrpProfile.Controls.Add(Me.Label1)
+        Me.GrpProfile.Controls.Add(Me.txtUsername)
+        Me.GrpProfile.Controls.Add(Me.lblUsername)
+        Me.GrpProfile.Location = New System.Drawing.Point(395, 344)
+        Me.GrpProfile.Name = "GrpProfile"
+        Me.GrpProfile.Size = New System.Drawing.Size(713, 239)
+        Me.GrpProfile.TabIndex = 2
+        Me.GrpProfile.TabStop = False
+        Me.GrpProfile.Text = "Profile Details"
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnExit.ForeColor = System.Drawing.Color.Red
+        Me.btnExit.Location = New System.Drawing.Point(546, 176)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(147, 48)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnLogin.ForeColor = System.Drawing.Color.Red
+        Me.btnLogin.Location = New System.Drawing.Point(35, 176)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(137, 50)
+        Me.btnLogin.TabIndex = 4
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'txtPassword
+        '
+        Me.txtPassword.Location = New System.Drawing.Point(166, 115)
+        Me.txtPassword.Name = "txtPassword"
+        Me.txtPassword.Size = New System.Drawing.Size(270, 23)
+        Me.txtPassword.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(35, 106)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 25)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Password:"
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Location = New System.Drawing.Point(166, 52)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(270, 23)
+        Me.txtUsername.TabIndex = 1
+        '
+        'lblUsername
+        '
+        Me.lblUsername.AutoSize = True
+        Me.lblUsername.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.lblUsername.ForeColor = System.Drawing.Color.Yellow
+        Me.lblUsername.Location = New System.Drawing.Point(35, 47)
+        Me.lblUsername.Name = "lblUsername"
+        Me.lblUsername.Size = New System.Drawing.Size(106, 25)
+        Me.lblUsername.TabIndex = 0
+        Me.lblUsername.Text = "Username:"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Gaming_Interface.My.Resources.Resources.images__1_1
+        Me.PictureBox1.Location = New System.Drawing.Point(535, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(406, 107)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'frmProfile
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Gaming_Interface.My.Resources.Resources.a77438db_e4f8_4d30_92b4_5b26f246219f
+        Me.ClientSize = New System.Drawing.Size(1489, 676)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.GrpProfile)
+        Me.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmProfile"
+        Me.Text = "Your Profile"
+        Me.GrpProfile.ResumeLayout(False)
+        Me.GrpProfile.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GrpProfile As GroupBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Gaming Interface/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmProfile
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click 'When The Login Button Is Pressed Login To Your Account'
+        MessageBox.Show("You Have Successfully Logged Into Your Account") 'Prompts The End User That They Have Logged Into There Account'
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click 'When The Exit Button Is Pressed It Exits The Program'
+        MessageBox.Show("Are You Sure You Want To Exit") 'Prompts The End User With A Confirmation If They Want To Leave The Program Or Not'
+        End
+    End Sub
+End Class
